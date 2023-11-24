ce08838b4b9af7fba471bc890b4a5707f5c60d24
Euphrosine <euphrosine46@.com>
2023-11-24 03:36:23 -0800

    changes

--- storeroom_project/settings.py
+++ storeroom_project/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  #always make sure you have this before deploying for static files to load
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
